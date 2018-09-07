ad2eacffb5fab0595eb6be7f2da566221b636104
wugl <309693441@qq.com>
2018-09-07 17:28:19 +0800

    update tmplateList page

--- pages/template_list/template_list.js
+++ pages/template_list/template_list.js
@@ -1,5 +1,57 @@
 const app = getApp()
 
 Page({
-  
+  data: {
+    cateInfo: [
+      {
+        catalog_id: '1',
+        catalog_name: '万科',
+        product_list: [
+          {
+            product_name: '万科aaa',
+            is_create_member: true,
+            is_confirm_split: false,
+            split_rule_id: '111'
+          },
+          {
+            product_name: '万科bbb',
+            is_create_member: true,
+            is_confirm_split: false,
+            split_rule_id: '222'
+          },
+          {
+            product_name: '万科ccc',
+            is_create_member: true,
+            is_confirm_split: false,
+            split_rule_id: '333'
+          }
+        ]
+      },
+      {
+        catalog_id: '1',
+        catalog_name: '美的项目',
+        product_list: [
+          {
+            product_name: '美团aaa',
+            is_create_member: true,
+            is_confirm_split: false,
+            split_rule_id: '111'
+          },
+          {
+            product_name: '美团bbb',
+            is_create_member: true,
+            is_confirm_split: false,
+            split_rule_id: '222'
+          },
+          {
+            product_name: '美团ccc',
+            is_create_member: true,
+            is_confirm_split: false,
+            split_rule_id: '333'
+          }
+        ]
+      },
+      
+    ]
+  }
 })
