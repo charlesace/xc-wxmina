756f5fb37e7e8dd97a78c9787ff9f0e24e866cc3
胡卓萌 <285261254@qq.com>
2018-09-13 17:14:13 +0800

    login css

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,63 +3,72 @@ const login = require('../../models/login.js')
 const util = require('../../utils/util.js')
 
 Page({
-  data: {
-    username: '',
-    password: '',
-    code: '',
-  },
-  onGotUserInfo (userInfo) {
+    data: {
+        username: '',
+        password: '',
+        code: '',
+        pwdFocus: false
+    },
 
-    util.login().then((res) => {
-      let code = res.code
+    onAccountConfirm() {
+        this.setData({
+            pwdFocus: true
+        })
+    },
 
-      this.setData({
-        code: code
-      })
+    onGotUserInfo(userInfo) {
+        util.login().then((res) => {
+            let code = res.code
 
-      let username = this.data.username
-      let password = this.data.password
+            this.setData({
+                code: code
+            })
 
-      login.loginByXC({
-        username,
-        password,
-        app_id: '0'
-      }).then((res) => {
+            let username = this.data.username
+            let password = this.data.password
 
-      })
+            login.loginByXC({
+                username,
+                password,
+                app_id: '0'
+            }).then((res) => {
 
-      // login.getOpenID({
-      //     code_no: code
-      //   }).then((openID) => {
+            })
 
-      //   })
+            // login.getOpenID({
+            //     code_no: code
+            //   }).then((openID) => {
 
-      // login.loginByXC({
-      //   username,
-      //   password
-      // }).then((res) => {
-      //   console.log('success', res)
-      //   wx.switchTab({
-      //     url: '../template_list/template_list'
-      //   })
+            //   })
 
-      // }).catch((err) => {
-      //   console.log('error', err)
-      // })
-    })
-  },
-  bindinputUsername (event) {
-    let username = event.detail.value
+            // login.loginByXC({
+            //   username,
+            //   password
+            // }).then((res) => {
+            //   console.log('success', res)
+            //   wx.switchTab({
+            //     url: '../template_list/template_list'
+            //   })
 
-    this.setData({
-      username: username
-    })
-  },
-  bindinputPassword (event) {
-    let password = event.detail.value
+            // }).catch((err) => {
+            //   console.log('error', err)
+            // })
+        })
+    },
 
-    this.setData({
-      password: password
-    })
-  }
+    bindinputUsername(event) {
+        let username = event.detail.value
+
+        this.setData({
+            username: username
+        })
+    },
+
+    bindinputPassword(event) {
+        let password = event.detail.value
+
+        this.setData({
+            password: password
+        })
+    }
 })
