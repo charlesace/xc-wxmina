1fcc608b6dacf0f7357103cc69043f41aa3166e7
wugl <309693441@qq.com>
2018-09-14 10:46:35 +0800

    format

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ const api = require('./config/api.js')
 const user = require('./models/user.js')
 
 App({
-  onLaunch (options) {
-    let {
-      scene
-    } = options
-    
-    this.globalData.scene = scene
+    onLaunch (options) {
+        let {
+            scene
+        } = options
+        
+        this.globalData.scene = scene
 
-    util.checkSession().then(
-      //  login Status : login
-      res => {
-      }
-    ).catch((error) => {
-      // login Status : session timeout , relogin
-      util.login()
-    })
-  },
-  globalData: {
-    userInfo: {
-      nickname: '游客',
-      username: 'login'
-    },
-    scene: ''
-  }
+        util.checkSession().then(
+            //  login Status : login
+            res => {
+                }
+            ).catch((error) => {
+            // login Status : session timeout , relogin
+                util.login()
+            })
+        },
+    globalData: {
+        userInfo: {
+        nickname: '游客',
+        username: 'login'
+        },
+        scene: ''
+    }
 })
