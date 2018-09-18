c236b899d800f43e9ab07d84b04bc30401fc91ec
wugl <309693441@qq.com>
2018-09-18 15:29:26 +0800

    update searchpage

--- models/order.js
+++ models/order.js
@@ -6,30 +6,73 @@ const http = require('../utils/httpRequest.js')
 const login = require('./login.js')
 
 module.exports = {
-    //  获取搜索列表
-    getMemberSearchList: function (roleID, memberName) {
+    searchList: [],
+    selectItemID: '',
+    searchParams: {
+        roleID: '',
+        member_name: '',
+        startIndex: 0,
+        pageSize: 20
+    },
+    members: [],    //  用于创建订单
 
-        return http.request(
-            'employee.member.query', {
-                app_id: login.appId,
-                member_role: roleID,
-                member_name: memberName,
-                start_index: 0,
-                page_size: 100
-            }
-        )
+
+    //  请求获取搜索列表数据 （输入名称改变时）
+    getMemberSearchList: function (loadMore) {
+
+        return new Promise((resolve, reject) => {
+            http.request(
+                'employee.member.query', {
+                    app_id: login.appId,
+                    member_role: this.roleID,
+                    member_name: this.searchParams['member_name'],
+                    start_index: loadMore ? this.searchParams['startIndex'] : 0,
+                    page_size: this.searchParams['pageSize']
+                }
+            ).then((res) => {
+                if (loadMore) {
+                    this.searchParams['startIndex'] += this.searchParams['pageSize']
+                    this.searchList.push(...res['members'])
+                    this.updateSelectSearchList()
+                } else {
+                    this.searchParams['startIndex'] = this.searchParams['pageSize'] - 1
+                    this.searchList = res['members']
+                    this.updateSelectSearchList()
+                }
+                // console.log(this.searchList)
+                resolve()
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+
+        // return http.request(
+        //     'employee.member.query', {
+        //         app_id: login.appId,
+        //         member_role: this.roleID,
+        //         member_name: this.memberName,
+        //         start_index: startIndex,
+        //         page_size: this.searchParams.pageSize
+        //     }
+        // )
     },
 
-    getAllMemberList: function (roleID) {
-        return http.request(
-            'employee.member.query', {
-                app_id: login.appId,
-                member_role: roleID,
-                member_name: '',
-                start_index: 0,
-                page_size: 100
+    //  选择一项时，清空其他的
+    setSelectItem: function (id) {
+        this.selectItemID = id
+
+        this.updateSelectSearchList()
+    },
+    updateSelectSearchList: function () {
+        this.searchList = this.searchList.map((item) => {
+            if (item.id === this.selectItemID) {
+                item.selected = true
+                return item
+            } else {
+                item.selected = false
+                return item
             }
-        )
+        })
     },
 
     // 获取买家临时唯一编码
---
+++ pages/order/searchPage.js
@@ -0,0 +1,201 @@
+// pages/order/searchPage.js
+const template = require('../../models/template.js')
+const orderModel = require('../../models/order.js')
+const util = require('../../utils/util.js')
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        searchList: [{
+            name: 'hhh',
+            id: 0
+        }, {
+            name: '222',
+            id: 1
+        }],
+        currentIndex: 0,
+        inputVal: ''
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+        this.setData({
+            searchList: orderModel.searchList
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+        console.log('more info')
+        orderModel.getMemberSearchList(true).then(() => {
+            this.updateSearchList()
+        })
+        
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+
+    searchMember (event) {
+
+        let dataset = event['currentTarget']['dataset']
+        let search = dataset.search
+        let {
+            role_code,
+            role_name,
+            is_member_updatable
+        } = search
+
+        this.setData({
+            currentSearch: search
+        })
+
+        let memberName = this.data.searchName || ''
+
+        if (!is_member_updatable) {
+            return
+        }
+
+        this.showSearchPage()
+        wx.setNavigationBarTitle({title: role_name})
+
+        orderModel.getAllMemberList(role_code).then((result) => {
+            let searchList = result['members'] || []
+
+            this.setData({
+                searchList: searchList
+            })
+        })
+
+    },
+
+    showSearchPage: function () {
+        this.setData({
+            searchPageHidden: false
+        })
+    },
+    hideSearchPage: function () {
+        this.setData({
+            searchPageHidden: true
+        })
+    },
+    resetSearchData: function () {
+        this.setData({
+            searchList: []
+        })
+    },
+
+    showInput: function () {
+        this.setData({
+            inputShowed: true
+        })
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        })
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        })
+        let searchData = this.data.currentSearch
+        let {
+            role_code
+        } = searchData
+
+        orderModel.getAllMemberList(role_code).then((result) => {
+            let searchList = result['members'] || []
+
+            this.setData({
+                searchList: searchList
+            })
+        })
+    },
+    inputTyping: function (e) {
+        // let searchData = this.data.currentSearch
+        let inputVal = e.detail.value
+        orderModel.searchParams['member_name'] = inputVal
+        // this.setData({
+        //     inputVal: inputVal
+        // })
+
+        // orderModel.getMemberSearchList(inputVal).then(() => {
+        //     this.updateSearchList()
+        // })
+
+        orderModel.getMemberSearchList().then(() => {
+            this.updateSearchList()
+        })
+
+    },
+
+    chooseMember (event) {
+        let dataset = event['currentTarget']['dataset']
+        let currentSearch = this.data['currentSearch']
+        let chooseItem = dataset.item
+        // 根据 当前搜索项与点击的项， 设置
+        let members = this.data['members']
+
+        console.log(chooseItem)
+
+        orderModel.setSelectItem(chooseItem['id'])
+        this.updateSearchList()
+
+    },
+
+    updateSearchList () {
+        this.setData({
+            searchList: orderModel.searchList
+        })
+    }
+})
